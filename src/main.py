97a5e06b1b1359d4d08f3da9272b65635ede6b04
YARA-coder <?fYl^3aoBvk6^^ww3|+"RQaM*kGQ9"VG>
2022-01-05 03:51:25 +0100

    replace radiobuttons with comboboxes.  add 'fancy' switch for chords above

--- src/main.py
+++ src/main.py
@@ -14,8 +14,8 @@ from src.song import Song
 os.chdir(pathlib.Path(__file__).parent)
 
 designer = Designer()
-is_uic = designer.build('uic','../src/main_window.ui', '../src/ui_mainwindow.py')
-is_rcc = designer.build('rcc', '../img/icons.qrc', '../src/icons_rc.py')
+is_uic = designer.build('uic','main_window.ui', 'ui_mainwindow.py')
+is_rcc = designer.build('rcc', '../img/icons.qrc', 'icons_rc.py')
 from src.ui_mainwindow import Ui_MainWindow
 
 log.basicConfig(
@@ -30,6 +30,7 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.example = Example()
+        self.last_used_filepath = None
 
         # ------ EVENT Connections --------------------------------------------
         self._connect_events()
@@ -49,151 +50,108 @@ class MainWindow(QMainWindow):
         self.ui.actionHK.triggered.connect(self.menuExamples_hk)
         self.ui.actionOpen.triggered.connect(self.openFile)
         self.ui.actionSave.triggered.connect(self.saveAsFile)
-        self.ui.chb_chords_above.stateChanged.connect(self.onChangeChordsAbove)
-
-        self.ui.rb_in_ug.clicked.connect(self.__read_input_format)
-        self.ui.rb_in_chopro.clicked.connect(self.__read_input_format)
-        self.ui.rb_in_latex_leadsheets.clicked.connect(self.__read_input_format)
-        self.ui.rb_in_latex_songs.clicked.connect(self.__read_input_format)
-        self.ui.rb_in_hk.clicked.connect(self.__read_input_format)
-
-        self.ui.rb_out_ug.clicked.connect(self.__read_output_format)
-        self.ui.rb_out_chopro.clicked.connect(self.__read_output_format)
-        self.ui.rb_out_latex_leadsheets.clicked.connect(self.__read_output_format)
-        self.ui.rb_out_latex_songs.clicked.connect(self.__read_output_format)
-        self.ui.rb_out_hk.clicked.connect(self.__read_output_format)
+        self.ui.btn_chords_above.toggled.connect(self.onChangeChordsAbove)
+        self.ui.cbx_input_format.currentIndexChanged.connect(self.__read_input_format)
+        self.ui.cbx_output_format.currentIndexChanged.connect(self.__read_output_format)
 
     def _init_states(self):
         self.input_format = 0
-        self.ui.rb_in_ug.animateClick()
-        self.ui.rb_out_latex_leadsheets.animateClick()
-        self.ui.chb_chords_above.setChecked(False)
+        self.ui.cbx_input_format.setCurrentIndex(0)
+        self.ui.cbx_output_format.setCurrentIndex(2)
+        self.ui.btn_chords_above.setChecked(False)
         self.chords_above = 0
 
     def openFile(self):
-        fname = QFileDialog.getOpenFileName(self, "Open Image", ".", ("Songs (*.chopro *.crd *.tex *.sng, *.txt)"))
+        if not self.last_used_filepath:
+            self.last_used_filepath ='..'
+
+        fname = QFileDialog.getOpenFileName(self, 
+            caption="Open song file",
+            dir=self.last_used_filepath,
+            filter=("Songs (*.chopro *.crd *.tex *.sng *.txt)"))
+
+        self.last_used_filepath = pathlib.PurePath(fname[0]).parent.as_posix()
         ext = pathlib.PurePath(fname[0]).suffix
-        with open(file=fname[0], mode='r', encoding='utf-8') as f:
-            data = f.read()
+        try:
+            with open(file=fname[0], mode='r', encoding='utf-8') as f:
+                data = f.read()
         
-        self.ui.text_in.setPlainText(data)
-
-        if ext == '.tex':
-            self.__selectInputRadioByIndex(2)
-        elif ext == '.chopro':
-            self.__selectInputRadioByIndex(1)
-        elif ext == '.sng':
-            self.__selectInputRadioByIndex(4)
+        except FileNotFoundError:
+            log.warning('File opening failed.')
+        
         else:
-            self.__selectInputRadioByIndex(0)
+            self.ui.text_in.setPlainText(data)
 
-        log.debug(f'openinig file: {fname[0]}')
+            if ext == '.tex':
+                self.ui.cbx_input_format.setCurrentIndex(2)
+
+            elif ext == '.chopro':
+                self.ui.cbx_input_format.setCurrentIndex(1)
+
+            elif ext == '.sng':
+                self.ui.cbx_input_format.setCurrentIndex(4)
+
+            else:
+                self.ui.cbx_input_format.setCurrentIndex(0)
+
+            log.debug(f'openinig file: {fname[0]}')
 
     def saveAsFile(self):
         formats = ['Ultimate Guitar (*.txt)', 'ChoPro (*.chopro', 'LaTeX (*.tex)','LaTeX sOngs (*.tex)' 'HK (*.sng)']
 
         fname = QFileDialog.getSaveFileName('Save as...', 'capotion', '.', formats[self.output_format])
         log.debug(f'Save as... {fname}')
-
-    def __selectInputRadioByIndex(self, idx):
-        if idx == 0:
-            self.ui.rb_in_ug.animateClick()
-        elif idx == 1:
-            self.ui.rb_in_chopro.animateClick()
-        elif idx == 2:
-            self.ui.rb_in_latex_leadsheets.animateClick()
-        elif idx == 3:
-            self.ui.rb_in_latex_songs.animateClick()
-        elif idx == 4:
-            self.ui.rb_in_hk.animateClick()
-
+# ------------------------------------------------------------------------------
     def menuExamples_ug(self):
-        self.ui.rb_in_ug.animateClick()
         self.load_example(0)
 
     def menuExamples_chopro(self):
-        self.ui.rb_in_chopro.animateClick()
         self.load_example(1)
 
     def menuExamples_latex_leadsheets(self):
-        self.ui.rb_in_latex_leadsheets.animateClick()
         self.load_example(2)
 
     def menuExamples_latex_songs(self):
-        self.ui.rb_in_latex_songs.animateClick()
         self.load_example(3)
 
     def menuExamples_hk(self):
-        self.ui.rb_in_hk.animateClick()
         self.load_example(4)
-
+# ------------------------------------------------------------------------------
     def load_example(self, song_format=None):
         if song_format != None:
             self.ui.text_in.setPlainText(self.example.frm(song_format))
-
+            self.ui.cbx_input_format.setCurrentIndex(song_format)
+            self.convert()
             log.debug('')
 
     def _check_in_out(self):
         pass
 
-    def __read_input_format(self, state):
-        if self.ui.rb_in_ug.isChecked():
-            self.input_format = 0
-        elif self.ui.rb_in_chopro.isChecked():
-            self.input_format = 1
-        elif self.ui.rb_in_latex_leadsheets.isChecked():
-            self.input_format = 2
-        elif self.ui.rb_in_latex_songs.isChecked():
-            self.input_format = 3
-        elif self.ui.rb_in_hk.isChecked():
-            self.input_format = 4
-            
-
-        rbs = [self.ui.rb_out_ug,self.ui.rb_out_chopro,self.ui.rb_out_latex_leadsheets,self.ui.rb_out_latex_songs,self.ui.rb_out_hk]
-        rbs_states = [1,1,1,0,1]
-        rbs_states[self.input_format] = False
-        
-        ''' THIS SHOULD BE REDESIGNED. SAMEOUTPUTS POSSIBLE FOR ABOVE/INLINE conversion eg. 
-        # disable same output...
-        for rb in rbs:
-            idx = rbs.index(rb)
-            rb.setEnabled(rbs_states[idx])
-
-        # ...and select next one
-        next_idx = ((self.input_format+1 -len(rbs)) % len(rbs))
-        rbs[next_idx].setChecked(True) 
-        '''
-
+    def __read_input_format(self, idx):
+        self.input_format = idx
         self.convert()
 
         log.debug(f'self.input_format= {self.input_format}')
 
-    def __read_output_format(self, state):
-        if self.ui.rb_out_ug.isChecked():
-            self.output_format = 0
-        elif self.ui.rb_out_chopro.isChecked():
-            self.output_format = 1
-        elif self.ui.rb_out_latex_leadsheets.isChecked():
-            self.output_format = 2
-        elif self.ui.rb_out_latex_songs.isChecked():
-            self.output_format = 3
-        elif self.ui.rb_out_hk.isChecked():
-            self.output_format = 4
+    def __read_output_format(self, idx):
+        # use Combobox to set format
+        self.output_format = idx
 
         if self.output_format == 4:
-            self.ui.chb_chords_above.setDisabled(True)
-            self.ui.chb_chords_above.setChecked(False)
+            self.ui.btn_chords_above.setDisabled(True)
+            self.ui.btn_chords_above.setChecked(False)
+
         else:
-            self.ui.chb_chords_above.setDisabled(False)
+            self.ui.btn_chords_above.setDisabled(False)
 
         self.convert()
 
         log.debug(f'self.output_format= {self.output_format}')
-
-    def onChangeChordsAbove(self, state):
-        self.chords_above = state
-        self.convert()
+    
+    def onChangeChordsAbove(self, checked):
+        self.chords_above = checked
         log.debug(f'self.chords_above= {self.chords_above}')
+        self.convert()
 
     def onChange_text_in(self):
         pass
@@ -203,20 +161,22 @@ class MainWindow(QMainWindow):
 
     def convert(self):
         if self.is_init_completed:
-            log.debug(
-                f'Convert triggered with options: IF: {self.input_format}   OF: {self.output_format}')
+            log.debug(f'Convert triggered with options: IF: {self.input_format}   OF: {self.output_format}')
             self.ui.statusbar.showMessage('converting', timeout=3000)
             in_txt = self.ui.text_in.toPlainText()
             if len(in_txt) > 3:
-
                 try:
                     s = Song(raw_text=in_txt, input_format=self.input_format)
                     out_txt = s.convert(to_format=self.output_format, chords_above=self.chords_above)
-                except (UnboundLocalError, AttributeError):
+                
+                except (UnboundLocalError, AttributeError) as err:
+                    log.error(err)
                     out_txt = 'ERROR!'
                     self.ui.statusbar.showMessage('ERROR!', timeout=3000)
+                
                 else:
                     pass
+                
                 finally:
                     self.ui.text_out.setPlainText(out_txt)
 
