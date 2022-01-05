fa75c5b1adc004c604bfd186820adaf86a7c5fcd
YARA-coder <?fYl^3aoBvk6^^ww3|+"RQaM*kGQ9"VG>
2022-01-05 03:46:43 +0100

    remove logging setup from chords.py

--- src/chord.py
+++ src/chord.py
@@ -5,9 +5,6 @@ sys.path.insert(0, '..')
 
 from src.regex_patterns import *
 
-log.basicConfig(
-    level=log.WARNING,
-    format='%(levelname)s: %(module)s.%(funcName)s(); Message: %(message)s')
 
 class Chord:
     '''class decodes chord from txt format and '''
